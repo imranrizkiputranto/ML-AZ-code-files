8deddc3fdf0a72ae1c5c618ed5f0737de8e0f769
Imran Rizki Putranto <81834185+imranrizkiputranto@users.noreply.github.com>
2023-05-22 14:37:38 +0100

    Add files via upload

---
+++ Section 2 - Regression/RandomForestRegression/RandomForestRegression.py
@@ -0,0 +1,44 @@
+"""
+Random Forest Regression Model
+19 May 2023
+Imran Rizki Putranto
+
+Creating a random forest regression model relating an individual's position and their salaries
+"""
+
+# Importing necessary libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import sys
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestRegressor
+import matplotlib
+matplotlib.use('TkAgg')
+np.set_printoptions(threshold=sys.maxsize)
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:-1].values
+Y = dataset.iloc[:, -1].values
+
+# Training Random Forest Regression model on dataset
+regressor = RandomForestRegressor(n_estimators=10, random_state=0)
+regressor.fit(X, Y)
+
+# Predicting new result
+print(regressor.predict([[6.5]]))
+
+# Visualising decision tree regression results
+fig = plt.figure()
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+Y_pred = regressor.predict(X_grid).reshape(-1, 1)
+ax2 = fig.add_subplot(111)
+ax2.scatter(X, Y, color='red')
+ax2.plot(X_grid, Y_pred, color='blue')
+plt.title('Salaries Against Position (Random Forest Regression)')
+plt.xlabel('Position')
+plt.ylabel('Salary')
+plt.show()
+
